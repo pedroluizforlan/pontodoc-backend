cc11859b4544a96a1eeb2709fdf44b189d7da38c
pedroluizforlan <pedro.forlan2000@gmail.com>
2025-09-20 10:50:35 -0300

    Uploading project to Github

---
+++ src/main/java/com/pedroluizforlan/pontodoc/PontodocApplication.java
@@ -0,0 +1,13 @@
+package com.pedroluizforlan.pontodoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontodocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontodocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroluizforlan/pontodoc/PontodocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroluizforlan.pontodoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontodocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
